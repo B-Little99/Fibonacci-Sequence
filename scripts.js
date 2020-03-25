3c72484aa9f98ce4b383305eb9772e33865e9687
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-25 19:29:02 +0000

    Working on improving the JavaScript

--- scripts.js
+++ scripts.js
@@ -4,19 +4,16 @@ function fSequence() {
     let arr = [];
     let input = document.getElementById("input").value;
 
-    if (arr.length === 0) {
-        arr.push(1);
-        arr.push(1);
-    }
 /* 
 The above code adds the first two numbers to the array used for the sequence. Since it was only two numbers I did not feel that a loop was necessary in this instance.
 */
-    for (i = 0; i < arr.length; i++) {
-        number = (arr[0 + i]) + (arr[1 + i]);
-        arr.push(number);
-        if (arr.length >= input) {
-            break;
+    for (i = 0; i < input; i++) {
+        if (i === 0 || i === 1) {
+            arr.push(1);
         }
+        else (number = (arr[i]) + (arr[1 + i]))
+        arr.push(number);
+        {}
     }
     document.getElementById("resultSection").innerHTML = arr.join(", ");
 }
