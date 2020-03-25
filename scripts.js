d2d513eccc391c91a7ee1665faa08006a1dba40f
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-25 17:35:52 +0000

    Fixed issue with the reset button.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ function fSequence() {
         arr.push(1);
         arr.push(1);
     }
+/* 
+The above code adds the first two numbers to the array used for the sequence. Since it was only two numbers I did not feel that a loop was necessary in this instance.
+*/
     for (i = 0; i < arr.length; i++) {
         number = (arr[0 + i]) + (arr[1 + i]);
         arr.push(number);
@@ -18,6 +21,6 @@ function fSequence() {
     document.getElementById("resultSection").innerHTML = arr.join(", ");
 }
 
-function reset() {
-    location.reload(true);
-}
+document.getElementById("resetButton").addEventListener("click", function reset(){
+    location.reload()
+  });
