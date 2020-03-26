95a295c6bc5b246aff265e9218f5016b798bcb43
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-26 09:13:30 +0000

    Added CSS, README doc and cleaned up code.

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
+This programme allows the user to enter a number and have the program generate the Fibonacci sequence to the Nth number.
 
-
-
-
-Debugging:
-
-I encountered some bugs along the way.
-
-The first was that it did not 
+Working on this allowed me to develope my JavaScript and debugging skills.
--- scripts.js
+++ scripts.js
@@ -1,19 +1,16 @@
-
 function fSequence() {
     let number;
     let arr = [];
     let input = document.getElementById("input").value;
 
-/* 
-The above code adds the first two numbers to the array used for the sequence. Since it was only two numbers I did not feel that a loop was necessary in this instance.
-*/
     for (i = 0; i < input; i++) {
         if (i === 0 || i === 1) {
             arr.push(1);
         }
-        else (number = (arr[i]) + (arr[1 + i]))
-        arr.push(number);
-        {}
+        else { 
+            number = (arr[i - 2]) + (arr[i - 1])
+            arr.push(number);
+        }
     }
     document.getElementById("resultSection").innerHTML = arr.join(", ");
 }
